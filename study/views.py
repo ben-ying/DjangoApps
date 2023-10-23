d920869cf9a209ebe94d1fab7350dccbab8a0b96
benying1988@gmail.com <ben>
2023-10-23 16:31:01 +0800

    feat: import and export data unique by ID

--- study/views.py
+++ study/views.py
@@ -20,13 +20,27 @@ from .utils import get_question_verbose_name, get_choice_key_by_value
 
 
 def index(request):
+    messages = []
     if request.method == 'POST':
-        uploaded_file = request.FILES['excel_file']
+        uploaded_file = ''
+        try:
+            uploaded_file = request.FILES['excel_file']
+        except:
+            messages.append(f"{_('没有上传对应文件')}")
+            return render(request, 'index.html', {'messages': messages})
 
         if uploaded_file.name.endswith('.xlsx'):
             df = pd.read_excel(uploaded_file, engine='openpyxl', na_filter = False)
 
             for index, row in df.iterrows():
+                id = row['id']
+                print(f'id: {id}')
+                if Question.objects.filter(id=id):
+                    if len(messages) < 100:
+                        messages.append(f"Line {index + 2}: {_('ID已存在无法重复导入')}")
+                    elif len(messages) < 101:
+                        messages.append(f"......")
+                    continue
                 grade = row[get_question_verbose_name('grade')]
                 subject = get_choice_key_by_value(SUBJECT_CHOICES, row[get_question_verbose_name('subject')]) # value to key
                 title = row[get_question_verbose_name('title')]
@@ -52,7 +66,8 @@ def index(request):
                     empty_field = 'category'
                 
                 if empty_field:
-                    message = f"Line {index + 2}: \"{get_question_verbose_name(empty_field)}\" {_('不能为空')}"
+                    messages.clear()
+                    messages.append(f"Line {index + 2}: \"{get_question_verbose_name(empty_field)}\" {_('不能为空')}")
                     break
                 else:
                     Question.objects.create(
@@ -70,9 +85,14 @@ def index(request):
                         released=released,
                         creator=creator
                     )
-                    message = _('数据已成功导入！')
+                    if len(messages) < 10:
+                        messages.append(f"Line {index + 2}: {_('数据已成功导入！')}")
+                    elif len(messages) < 11:
+                        messages.append(f"......")
+        else:
+            messages = f"_('文件格式不正确')"
 
-    return render(request, 'index.html', {'message': message})
+    return render(request, 'index.html', {'messages': messages})
 
 
 def export_to_excel(request):
@@ -85,12 +105,14 @@ def export_to_excel(request):
     # Prepare data (as a list of dictionaries)
     data = [
                 {
-                    get_question_verbose_name(item, 'grade'): item.grade, get_question_verbose_name(item, 'subject'): item.subject, get_question_verbose_name(item, 'title'): item.title, \
-                    get_question_verbose_name(item, 'description_above_image'): item.description_above_image, get_question_verbose_name(item, 'image'): item.image, get_question_verbose_name(item, 'description_below_image'): item.description_below_image, \
-                    get_question_verbose_name(item, 'classroom_exercises'): item.classroom_exercises, get_question_verbose_name(item, 'score'): item.score, get_question_verbose_name(item, 'number_of_errors'): item.number_of_errors, \
-                    get_question_verbose_name(item, 'category'): item.category, get_question_verbose_name(item, 'fixed'): item.fixed, get_question_verbose_name(item, 'exam_name'): item.exam_name, \
-                    get_question_verbose_name(item, 'released'): item.released, get_question_verbose_name(item, 'creator'): item.creator, get_question_verbose_name(item, 'created'): item.created, \
-                    get_question_verbose_name(item, 'modified'): item.modified
+                    "id": item.id, get_question_verbose_name('grade'): item.grade, get_question_verbose_name('subject'): item.subject, \
+                    get_question_verbose_name('title'): item.title, get_question_verbose_name('description_above_image'): item.description_above_image, \
+                    get_question_verbose_name('image'): item.image, get_question_verbose_name('description_below_image'): item.description_below_image, \
+                    get_question_verbose_name('classroom_exercises'): item.classroom_exercises, get_question_verbose_name('score'): item.score, \
+                    get_question_verbose_name('number_of_errors'): item.number_of_errors, get_question_verbose_name('category'): item.category, \
+                    get_question_verbose_name('fixed'): item.fixed, get_question_verbose_name('exam_name'): item.exam_name, \
+                    get_question_verbose_name('released'): item.released, get_question_verbose_name('creator'): item.creator, \
+                    get_question_verbose_name('created'): item.created, get_question_verbose_name('modified'): item.modified
                 } for item in queryset
             ]
     
@@ -104,50 +126,53 @@ def export_to_excel(request):
 
     # Process and insert images into Excel
     for row, item in enumerate(data, start=2):
-        # grade
+        # id
         sheet.cell(row=row, column=1, value=item[list(item)[0]])
+        # grade
+        sheet.cell(row=row, column=2, value=item[list(item)[1]])
         # subject
-        sheet.cell(row=row, column=2, value=SUBJECT_CHOICES[item[list(item)[1]] - 1][1]) # Change to choice value
+        sheet.cell(row=row, column=3, value=SUBJECT_CHOICES[item[list(item)[2]] - 1][1]) # Change to choice value
         # title
-        sheet.cell(row=row, column=3, value=item[list(item)[2]])
-        # description_above_image
         sheet.cell(row=row, column=4, value=item[list(item)[3]])
+        # description_above_image
+        sheet.cell(row=row, column=5, value=item[list(item)[4]])
         # description_below_image
-        sheet.cell(row=row, column=6, value=item[list(item)[5]])
-        # classroom_exercises
         sheet.cell(row=row, column=7, value=item[list(item)[6]])
-        # score
+        # classroom_exercises
         sheet.cell(row=row, column=8, value=item[list(item)[7]])
-        # number_of_errors
+        # score
         sheet.cell(row=row, column=9, value=item[list(item)[8]])
+        # number_of_errors
+        sheet.cell(row=row, column=10, value=item[list(item)[9]])
         # category
-        sheet.cell(row=row, column=10, value=CATEGORY_CHOICES[item[list(item)[9]] - 1][1])
+        sheet.cell(row=row, column=11, value=CATEGORY_CHOICES[item[list(item)[10]] - 1][1])
         # fixed
-        sheet.cell(row=row, column=11, value=item[list(item)[10]])
-        # exam_name
         sheet.cell(row=row, column=12, value=item[list(item)[11]])
-        # released
+        # exam_name
         sheet.cell(row=row, column=13, value=item[list(item)[12]])
-        # creator
+        # released
         sheet.cell(row=row, column=14, value=item[list(item)[13]])
-        # created
+        # creator
         sheet.cell(row=row, column=15, value=item[list(item)[14]])
-        # modified
+        # created
         sheet.cell(row=row, column=16, value=item[list(item)[15]])
+        # modified
+        sheet.cell(row=row, column=17, value=item[list(item)[16]])
 
         # iamge
-        img = PILImage.open(item[list(item)[4]])
-        # img = img.resize((100, 100))  # Resize the image as needed
+        if item[list(item)[5]]:
+            img = PILImage.open(item[list(item)[5]])
+            # img = img.resize((100, 100))  # Resize the image as needed
 
-        img_io = BytesIO()
-        img.save(img_io, 'PNG')
+            img_io = BytesIO()
+            img.save(img_io, 'PNG')
 
-        img_path = f'media/study/images/tmp_image_{row}.png'
-        with open(img_path, 'wb') as f:
-            f.write(img_io.getvalue())
+            img_path = f'media/study/images/tmp_image_{row}.png'
+            with open(img_path, 'wb') as f:
+                f.write(img_io.getvalue())
 
-        img = Image(img_path)
-        sheet.add_image(img, f'E{row}') # E means column=5
+            img = Image(img_path)
+            sheet.add_image(img, f'F{row}') # E means column=6
 
     for index, row in enumerate(sheet.iter_rows()):
         # Set row height, title no need to set
@@ -159,9 +184,9 @@ def export_to_excel(request):
             cell.alignment = Alignment(wrap_text=True, vertical='center', horizontal='center')
                 
     # Set row width
-    sheet.column_dimensions['D'].width = 50 # description_above_image
-    sheet.column_dimensions['E'].width = 80 # image
-    sheet.column_dimensions['F'].width = 50 # description_above_image
+    sheet.column_dimensions['E'].width = 50 # description_above_image
+    sheet.column_dimensions['F'].width = 80 # image
+    sheet.column_dimensions['G'].width = 50 # description_above_image
     # Save the Excel file
     workbook.save('media/study/files/output.xlsx')
     # Create a response with the Excel file
